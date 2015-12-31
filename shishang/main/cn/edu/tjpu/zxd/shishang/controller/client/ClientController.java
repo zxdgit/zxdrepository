b893c0e0d097d4ccb118ebd293c23fcd2245d8f6
zxdgit <1527681789@qq.com>
2015-12-31 13:26:48 +0800

    实验

--- shishang/main/cn/edu/tjpu/zxd/shishang/controller/client/ClientController.java
+++ shishang/main/cn/edu/tjpu/zxd/shishang/controller/client/ClientController.java
@@ -1,13 +1,23 @@
 package cn.edu.tjpu.zxd.shishang.controller.client;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.edu.tjpu.zxd.shishang.model.User;
+import cn.edu.tjpu.zxd.shishang.service.UserService;
 
 @Controller
 public class ClientController {
 
+	@Autowired
+	UserService service;
 	
+	@RequestMapping("/test")
+	public String test(){
+		return "client/index";
+	}
 	
 }
